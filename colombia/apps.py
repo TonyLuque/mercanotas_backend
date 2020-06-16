d1f4ef51b61580e0e2e598042328b66bf3a4d743
Anthony Stiven Luque <kasuru@hotmail.com>
2020-06-15 22:02:10 -0500

    [ADD] h1

--- colombia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ColombiaConfig(AppConfig):
-    name = 'colombia'
-    verbose_name = 'Colombia'
--- mercanotas_backend/urls.py
+++ mercanotas_backend/urls.py
@@ -19,47 +19,9 @@ from django.http import HttpResponse
 
 import json
 
-from colombia import views as colombia_views
-
 def hola(request):
-    return HttpResponse('Hola Beto como vas? \n si te llego el correo?')
-
-def prueba1(request):
-    return HttpResponse('la otra url Si corre Juy !!!!!!!!!!!!!!!!!!!! :D')
-
-def prueba2(request):
-    data = {
-        'status': 'ok',
-        'country': request.GET['country'],
-        'message': 'Tu pais es muy bonito'
-    }
-    print(request.GET['country'])
-    return HttpResponse(json.dumps(data), content_type='application/json')
-    
-def prueba3(request, country):
-    data = {
-        'status': 'ok',
-        'country': country,
-        'message': 'Tu pais es muy bonito'
-    }
-    return HttpResponse(json.dumps(data), content_type='application/json')
-
-def prueba4(request, country, depart):
-    data = {
-        'status': 'ok',
-        'country': country,
-        'depart': depart,
-        'message': 'Tu pais es muy bonito'
-    }
-    return HttpResponse(json.dumps(data), content_type='application/json')
+    return HttpResponse("""<h1>Hola soy un HTML</h1>""")
 
 urlpatterns = [
     path('', hola),
-    path('prueba1/', prueba1),
-    path('pais/', prueba2),
-    path('pais/<str:country>', prueba3),
-    path('pais/<str:country>/<str:depart>', prueba4),
-
-    path('colombia/', colombia_views.list_cities),
-    path('colombia/<str:city>', colombia_views.view_city),
 ]
